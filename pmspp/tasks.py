096a4a88c87ee250018a4614fca8243527771df7
Rad Cirskis <nad2000@gmail.com>
2020-04-04 22:50:18 +1300

    logging for tracing

--- pmspp/tasks.py
+++ pmspp/tasks.py
@@ -1,4 +1,5 @@
 from background_task import background
+from background_task.tasks import logger
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -9,4 +10,4 @@ def notify_user(user_id, message):
     # lookup user by id and send them a message
     user = User.objects.get(pk=user_id)
     user.email_user("Here is a notification", message or "You have been notified")
-    print(f"*** Message '{message}' was sent to address {user.email}")
+    logger.info(f"*** Message '{message}' was sent to address {user.email}")
