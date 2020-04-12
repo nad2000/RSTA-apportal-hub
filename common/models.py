f36c09de18f6ce4a06e1412cb2d77b35121e6d10
Rad Cirskis <nad2000@gmail.com>
2020-04-12 15:56:17 +1200

    missing bit ../.. common moduls

---
+++ common/models.py
@@ -0,0 +1,54 @@
+from django.db.models import DateTimeField
+from django.db.models import Model as _Model
+from model_utils import Choices
+from simple_history.models import HistoricalRecords
+
+SEX_CHOICES = Choices("female", "male", "other")
+
+ETHNICITY_COICES = Choices(
+    "European",
+    "Maori",
+    "Chinese",
+    "Indian",
+    "Samoan",
+    "Tongan",
+    "Cook Islands Maori",
+    "English",
+    "Filipino",
+    "New Zealander",
+    "Other",
+)
+
+
+class TimeStampMixin(_Model):
+    created_at = DateTimeField(auto_now_add=True, null=True)
+    updated_at = DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class HelperMixin:
+    @classmethod
+    def first(cls):
+        return cls.objects.first()
+
+    @classmethod
+    def last(cls):
+        return cls.objects.last()
+
+    @classmethod
+    def get(cls, *args, **kwargs):
+        if args:
+            return cls.objects.get(pk=args[0])
+        else:
+            return cls.objects.get(**kwargs)
+        return cls.objects.first()
+
+
+class Model(TimeStampMixin, HelperMixin, _Model):
+
+    history = HistoricalRecords(inherit=True)
+
+    class Meta:
+        abstract = True
