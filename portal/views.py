7b0ca694b73c0540e99ad240fc6baec92794a0ac
rpaw053 <getconnectedtoroshan@gmail.com>
2020-05-20 01:49:47 +1200

    adding more changes

--- portal/views.py
+++ portal/views.py
@@ -400,6 +400,7 @@ class ProfileAffiliationsFormSetView(ProfileSectionFormSetView):
         """Check the POST request call """
         if "load_from_orcid" in request.POST:
             social_accounts = self.request.user.socialaccount_set.all()
+            user_has_linked_orcid = False
             for sa in social_accounts:
                 if sa.provider == "orcid" and sa.extra_data:
                     at = "employment" if self.affiliation_type == "EMP" else "education"
@@ -407,18 +408,28 @@ class ProfileAffiliationsFormSetView(ProfileSectionFormSetView):
                         ag for ag in sa.extra_data.get(
                             "activities-summary").get(f"{at}s").get(f"{at}-summary")
                     ]
-
+                    count=0
                     for aff in affiliation_objs:
                         org, _ = models.Organisation.objects.get_or_create(name=aff.get('organization').get('name'))
                         org.save()
-                        affiliation_obj = self.model.objects.create(profile=self.request.user.profile, org=org)
-                        affiliation_obj.type = self.affiliation_type
-                        affiliation_obj.role = aff.get('role-title')
-                        if aff.get('start-date'):
-                            affiliation_obj.start_date = str(PartialDate.create(aff.get('start-date')))
-                        if aff.get('end-date'):
-                            affiliation_obj.end_date = str(PartialDate.create(aff.get('end-date')))
-                        affiliation_obj.save()
+                        affiliation_obj, status = self.model.objects.get_or_create(profile=self.request.user.profile,
+                                                                                   org=org,
+                                                                                   put_code=aff.get('put-code'))
+                        if status:
+                            affiliation_obj.type = self.affiliation_type
+                            affiliation_obj.role = aff.get('role-title')
+                            if aff.get('start-date'):
+                                affiliation_obj.start_date = str(PartialDate.create(aff.get('start-date')))
+                            if aff.get('end-date'):
+                                affiliation_obj.end_date = str(PartialDate.create(aff.get('end-date')))
+                            affiliation_obj.put_code = aff.get('put-code')
+                            affiliation_obj.save()
+                            count+=1
+                    messages.success(self.request, f" {count} {at} records from ORCID loaded!!")
+                    user_has_linked_orcid = True
+            if not user_has_linked_orcid:
+                messages.warning(self.request, f"Please link your ORCID Account!!")
+
 
         return super(ProfileAffiliationsFormSetView, self).post(request, *args, **kwargs)
 
