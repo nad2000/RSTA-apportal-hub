3a5dc9a1193d251e5436a5f463b6682e5d3159b6
rpaw053 <getconnectedtoroshan@gmail.com>
2020-06-17 04:37:55 +1200

    Enabling a way for administrator to approve the self signed user request

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,10 @@
 from captcha.fields import ReCaptchaField
 from django import forms as django_forms
+from django.conf import settings
 from django.contrib.auth import forms, get_user_model
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.shortcuts import reverse
 from django.utils.translation import ugettext_lazy as _
 
 User = get_user_model()
@@ -37,4 +40,20 @@ class UserSignupForm(django_forms.Form):
     def signup(self, request, user):
         user.is_approved = False
         user.save()
+
+        url = request.build_absolute_uri(
+            reverse("users:approve-user", kwargs=dict(user_id=user.id))
+        )
+
+        admin_users = User.where(is_staff=True)
+        admin_emails = []
+        for u in admin_users:
+            admin_emails.append(u.email)
+        send_mail(
+            _("[Prime Minister's Science Prizes] New User Signed up to join the portal"),
+            _(f"Please click on the link to approve user {user.email}: ") + url,
+            settings.DEFAULT_FROM_EMAIL,
+            recipient_list=admin_emails,
+            fail_silently=False,
+        )
         return user
