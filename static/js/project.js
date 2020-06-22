7a82c75de160782b62711f4b69e31ba7baf59c49
Rad Cirskis <nad2000@gmail.com>
2020-06-22 19:34:16 +1200

    fixed add row with formset prefix

--- static/js/project.js
+++ static/js/project.js
@@ -1,13 +1,15 @@
 /* Project specific Javascript goes here. */
-function formset_add_a_row(btn) {
+function formset_add_a_row(btn, prefix="form") {
   var root = btn.form;
   // var form_idx = $('form #id_form-TOTAL_FORMS').val();
-  var total = root.querySelector("#id_form-TOTAL_FORMS");
+  var total = root.querySelector("#id_" + prefix + "-TOTAL_FORMS");
   var form_idx = total.value;
 
-  var el = $('<tr>' + $('form #empty_form').html().replace(/__prefix__/g, form_idx) + '</tr>') ;
-  setDatePickers(el);
-  $('form #form_set').append(el);
+  // var el = $('<tr>' + $('form #empty_form').html().replace(/__prefix__/g, form_idx) + '</tr>') ;
+  var el = $('<tr>' + $(root).find('#empty_form').html().replace(/__prefix__/g, form_idx) + '</tr>') ;
+  if (typeof setDatePickers == 'function') setDatePickers(el);
+  // $('form #form_set').append(el);
+  $(root).find('#form_set').append(el);
   // root.querySelector("#form_set").append(el);
   //$('form #id_form-TOTAL_FORMS').val(parseInt(form_idx) + 1);
   total.value = parseInt(form_idx) + 1;
