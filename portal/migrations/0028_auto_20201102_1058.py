c8baaf09f6410fa82ad73fd2bb69732360021354
PMSP App <app@prod.prodata.org>
2020-11-02 22:09:59 +0000

    cannot turn off the foreign key constraints during migrations

--- portal/migrations/0028_auto_20201102_1058.py
+++ portal/migrations/0028_auto_20201102_1058.py
@@ -13,16 +13,16 @@ def disable_constraints(apps, schema_editor):
     engine = schema_editor.connection.settings_dict.get("ENGINE").split('.')[-1]
     if engine == 'sqlite3':
         schema_editor.execute("PRAGMA foreign_keys = OFF;")
-    else:
-        schema_editor.execute("SET FOREIGN_KEY_CHECKS=0;")
+    # else:
+    #     schema_editor.execute("SET FOREIGN_KEY_CHECKS=0;")
 
 
 def enable_constraints(apps, schema_editor):
     engine = schema_editor.connection.settings_dict.get("ENGINE").split('.')[-1]
     if engine == 'sqlite3':
         schema_editor.execute("PRAGMA foreign_keys = ON;")
-    else:
-        schema_editor.execute("SET FOREIGN_KEY_CHECKS=1;")
+    # else:
+    #     schema_editor.execute("SET FOREIGN_KEY_CHECKS=1;")
 
 
 class Migration(migrations.Migration):
